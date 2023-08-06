088ad1446c97599e58e34c1bf1d1a407ea8aab0c
jewc <jamescew@gmail.com>
2023-08-06 05:48:11 -0400

    Initial setup

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,30 @@
 const Quiz = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("0");
+  const { questions } = quiz;
+  const { question, choices } = questions[activeQuestion]; // destructuring
+
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
   });
-  return;
-  <div>
-    <h1>Quiz</h1>;
-  </div>;
+
+  const onClickNext = () => {
+    setActiveQuestion((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <h2>{question}</h2>
+      <ul>
+        {choices.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Quiz;
