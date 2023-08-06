72f455c6dcf362132e30c9d3429c082cf1f94464
jewc <jamescew@gmail.com>
2023-08-06 04:54:29 -0400

    First commit

---
+++ src/quiz.js
@@ -0,0 +1,15 @@
+const Quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("0");
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+  return;
+  <div>
+    <h1>Quiz</h1>;
+  </div>;
+};
+
+export default Quiz;
