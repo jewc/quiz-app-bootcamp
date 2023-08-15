2cab4cfa293acacc7fd693415fdbe646d8032a64
jewc <jamescew@gmail.com>
2023-08-15 08:26:08 -0400

    Updated quiz questions

--- src/data/quiz-questions.js
+++ src/data/quiz-questions.js
@@ -4,7 +4,7 @@
 export const quiz = {
   topic: "Javascript",
   level: "Beginner",
-  totalQuestions: 4,
+  totalQuestions: 10,
   perQuestionScore: 5,
   questions: [
     {
@@ -38,6 +38,63 @@ export const quiz = {
       choices: ["const", "var", "let", "constant"],
       type: "MCQs",
       correctAnswer: "const",
+    },
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+      type: "MCQs",
+      correctAnswer: "<script>",
+    },
+    {
+      question: "Where is the correct place to insert a JavaScript?",
+      choices: [
+        "The <body> section",
+        "Both the <head> section and the <body> section are correct",
+        "The <head> section",
+        "None of the above",
+      ],
+      type: "MCQs",
+      correctAnswer:
+        "Both the <head> section and the <body> section are correct",
+    },
+    {
+      question:
+        'What is the correct syntax for referring to an external script called "xxx.js"?',
+      choices: [
+        '<script name="xxx.js">  ',
+        '<script src="xxx.js">',
+        '<script href="xxx.js">',
+        "None of the above",
+      ],
+      type: "MCQs",
+      correctAnswer: '<script src="xxx.js">',
+    },
+    {
+      question: "The external JavaScript file must contain the <script> tag.",
+      choices: ["True", "False"],
+      type: "MCQs",
+      correctAnswer: "False",
+    },
+    {
+      question: 'How do you write "Hello World" in an alert box?',
+      choices: [
+        'alert("Hello World");',
+        'alertBox("Hello World");',
+        'msg("Hello World");',
+        'msgBox("Hello World");',
+      ],
+      type: "MCQs",
+      correctAnswer: 'alert("Hello World");',
+    },
+    {
+      question: "How do you create a function in JavaScript?",
+      choices: [
+        "function = myFunction()",
+        "function:myFunction()",
+        "function myFunction()",
+      ],
+      type: "MCQs",
+      correctAnswer: "function myFunction",
     },
   ],
 };
